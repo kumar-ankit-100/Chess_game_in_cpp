df4bc5828ef9ce193459e36ffbd73729c444d116
ankit <kumar.ankitr0321@gmail.com>
2024-10-02 22:52:28 +0530

    rook moves in left direction corrected

--- rook.cpp
+++ rook.cpp
@@ -10,10 +10,10 @@ int is_rook_move_valid(Board &board, string &moves, int flag)
         cout << "Please ! Enter valid rook move since rook move only in one direction " << endl;
         return 0;
     }
-    //this is the case for capturing pieces
-    // if(is_piece_present_in_square(board,moves[2],moves[3])){
-    //     return 5;
-    // }
+    // this is the case for capturing pieces
+    //  if(is_piece_present_in_square(board,moves[2],moves[3])){
+    //      return 5;
+    //  }
     if (moves[0] == moves[2])
     {
         // if file is same then check is there any pieces in between source and destination
@@ -24,7 +24,7 @@ int is_rook_move_valid(Board &board, string &moves, int flag)
         if (source <= destination)
         {
             source = source + 1;
-            destination=destination-1;
+            destination = destination - 1;
             cout << "rook is moving upward " << endl;
 
             while (source <= destination)
@@ -44,7 +44,7 @@ int is_rook_move_valid(Board &board, string &moves, int flag)
         else
         {
             source = source - 1;
-            destination = destination +1 ;
+            destination = destination + 1;
             cout << "rook is moving downward " << endl;
             while (source >= destination)
             {
@@ -70,7 +70,7 @@ int is_rook_move_valid(Board &board, string &moves, int flag)
         if (source <= destination)
         {
             source = source + 1;
-            destination = destination -1 ;
+            destination = destination - 1;
             cout << "rook is moving right " << endl;
             while (source <= destination)
             {
@@ -87,11 +87,12 @@ int is_rook_move_valid(Board &board, string &moves, int flag)
         }
         else
         {
+            source -= 1;
+            destination = destination + 1;
+            cout << "rook is moving left" << endl;
             while (source >= destination)
             {
-                source -= 1;
-                destination = destination + 1; 
-                cout << "rook is moving left" << endl;
+
                 Bitboard bit = 1;
                 Bitboard from_mask = bit << (((moves[1] - 48) * 8) - source - 1);
                 // checking that is piece is present in particular box or not
